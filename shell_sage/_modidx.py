471830f263da5f0745f8da2d42baebf2549332df
Isaac Flath <Isaac.flath@gmail.com>
2024-12-05 16:59:50 -0500

    Add option to select specific tmux pane id, and refactor along the way

--- shell_sage/_modidx.py
+++ shell_sage/_modidx.py
@@ -6,5 +6,7 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/AnswerDotAI/shell_sage',
                 'lib_path': 'shell_sage'},
   'syms': { 'shell_sage.core': { 'shell_sage.core.get_history': ('core.html#get_history', 'shell_sage/core.py'),
+                                 'shell_sage.core.get_pane_output': ('core.html#get_pane_output', 'shell_sage/core.py'),
+                                 'shell_sage.core.get_pane_outputs': ('core.html#get_pane_outputs', 'shell_sage/core.py'),
                                  'shell_sage.core.main': ('core.html#main', 'shell_sage/core.py'),
                                  'shell_sage.core.run_cmd': ('core.html#run_cmd', 'shell_sage/core.py')}}}
