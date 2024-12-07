88a0d1c5ccfd15811316ab40ea9bb5c47d9a3fa2
ncoop57 <nc@answer.ai>
2024-12-07 10:41:25 -0600

    Refactor tmux pane output function names

--- shell_sage/_modidx.py
+++ shell_sage/_modidx.py
@@ -6,6 +6,6 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/AnswerDotAI/shell_sage',
                 'lib_path': 'shell_sage'},
   'syms': { 'shell_sage.core': { 'shell_sage.core.get_history': ('core.html#get_history', 'shell_sage/core.py'),
-                                 'shell_sage.core.get_pane_output': ('core.html#get_pane_output', 'shell_sage/core.py'),
-                                 'shell_sage.core.get_pane_outputs': ('core.html#get_pane_outputs', 'shell_sage/core.py'),
+                                 'shell_sage.core.get_pane': ('core.html#get_pane', 'shell_sage/core.py'),
+                                 'shell_sage.core.get_panes': ('core.html#get_panes', 'shell_sage/core.py'),
                                  'shell_sage.core.main': ('core.html#main', 'shell_sage/core.py')}}}
